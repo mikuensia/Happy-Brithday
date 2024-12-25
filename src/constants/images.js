ea9f620a455ff6c1958ee55d420fd5dda87b5fb3
mikuensia <154864260+mikuensia@users.noreply.github.com>
2024-12-25 12:50:36 +0700

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,51 @@
+import Img14 from "../assets/pics/14.jpeg";
+import Img15 from "../assets/pics/15.jpeg";
+import Img16 from "../assets/pics/16.jpeg";
+import Img17 from "../assets/pics/17.jpeg";
+import Img18 from "../assets/pics/18.jpeg";
+import Img19 from "../assets/pics/19.jpeg";
+import Img20 from "../assets/pics/20.jpg";
+import Img21 from "../assets/pics/21.jpg";
+import Img22 from "../assets/pics/22.jpg";
+import Img23 from "../assets/pics/23.jpg";
+import Img24 from "../assets/pics/24.jpg";
+import Img25 from "../assets/pics/25.jpg";
+
+export const images = [
+  {
+    src: Img14,
+  },
+  {
+    src: Img15,
+  },
+  {
+    src: Img16,
+  },
+  {
+    src: Img17,
+  },
+  {
+    src: Img18,
+  },
+  {
+    src: Img19,
+  },
+  {
+    src: Img20,
+  },
+  {
+    src: Img21,
+  },
+  {
+    src: Img22,
+  },
+  {
+    src: Img23,
+  },
+  {
+    src: Img24,
+  },
+  {
+    src: Img25,
+  },
+];
